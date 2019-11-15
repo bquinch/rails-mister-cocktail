94fe943f194229dbe7c2cb53a77ee05c67631100
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-15 12:47:51 +0100

    heroku-2

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,5 +3,4 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true, allow_blank: false
-  validates :photo, presence: true
 end
