1d12408be921ff176bca0ec53f80aad8cae48e7c
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-16 13:32:17 +0100

    push

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,7 @@
 class Cocktail < ApplicationRecord
   has_one_attached :photo
   has_many :doses, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true, allow_blank: false
   validates :photo, presence: true
