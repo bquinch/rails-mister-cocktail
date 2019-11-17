6ddc045c4fd68de9b78adc4ba128288ff00afd52
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-17 14:37:59 +0100

    zoom in hover on image

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,46 +9,46 @@
 require 'open-uri'
 require 'json'
 
-# ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
-# # puts ingredients
-# ingredients['drinks'].each do |ingredient|
-#   newingredient = Ingredient.new(name: ingredient["strIngredient1"])
-#   newingredient.save!
-# end
+ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+# puts ingredients
+ingredients['drinks'].each do |ingredient|
+  newingredient = Ingredient.new(name: ingredient["strIngredient1"])
+  newingredient.save!
+end
 
-# alphabet = ('a'..'z').to_a
-# alphabet.each do |letter|
-#   unless JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}").read)['drinks'].nil?
-#     cocktails = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}").read)['drinks']
-#     cocktails.each do |cocktail|
-#       some_cocktail = Cocktail.find_by(name: cocktail['strDrink'])
-#       if some_cocktail.nil?
-#         my_cocktail = Cocktail.new(name: cocktail['strDrink'], instruction: cocktail['strInstructions'].gsub(/([\n|\r])/, ''))
-#         puts "Adding #{my_cocktail.name} to the database"
-#         file = open(cocktail['strDrinkThumb'])
-#         my_cocktail.photo.attach(io: file, filename: "#{cocktail['strDrinkThumb'][-15..-1]}")
-#         if my_cocktail.valid?
-#           puts "It's valid !"
-#           my_cocktail.save
-#           numbers = (1..5).to_a
-#           numbers.each do |number|
-#             unless cocktail["strMeasure#{number}"].nil? || Ingredient.find_by(name: cocktail["strIngredient#{number}"]).nil?
-#               dose = Dose.new(description: cocktail["strMeasure#{number}"])
-#               ingredient = Ingredient.find_by(name: cocktail["strIngredient#{number}"])
-#               dose.cocktail = my_cocktail
-#               dose.ingredient = ingredient
-#               if dose.valid?
-#                 dose.save!
-#               end
-#             end
-#           end
-#         end
-#       else
-#         puts "#{some_cocktail.name} already exists !"
-#       end
-#     end
-#   end
-# end
+alphabet = ('a'..'z').to_a
+alphabet.each do |letter|
+  unless JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}").read)['drinks'].nil?
+    cocktails = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}").read)['drinks']
+    cocktails.each do |cocktail|
+      some_cocktail = Cocktail.find_by(name: cocktail['strDrink'])
+      if some_cocktail.nil?
+        my_cocktail = Cocktail.new(name: cocktail['strDrink'], instruction: cocktail['strInstructions'].gsub(/([\n|\r])/, ''))
+        puts "Adding #{my_cocktail.name} to the database"
+        file = open(cocktail['strDrinkThumb'])
+        my_cocktail.photo.attach(io: file, filename: "#{cocktail['strDrinkThumb'][-15..-1]}")
+        if my_cocktail.valid?
+          puts "It's valid !"
+          my_cocktail.save
+          numbers = (1..5).to_a
+          numbers.each do |number|
+            unless cocktail["strMeasure#{number}"].nil? || Ingredient.find_by(name: cocktail["strIngredient#{number}"]).nil?
+              dose = Dose.new(description: cocktail["strMeasure#{number}"])
+              ingredient = Ingredient.find_by(name: cocktail["strIngredient#{number}"])
+              dose.cocktail = my_cocktail
+              dose.ingredient = ingredient
+              if dose.valid?
+                dose.save!
+              end
+            end
+          end
+        end
+      else
+        puts "#{some_cocktail.name} already exists !"
+      end
+    end
+  end
+end
 
 cocktail_list = Cocktail.all
 cocktail_list.each do |c|
@@ -59,9 +59,7 @@ cocktail_list.each do |c|
       rating: rand(1..5)
     )
     review.cocktail = c
-    if review.valid?
-      review.save!
-    end
+    review.save! if review.valid?
   end
 end
 
